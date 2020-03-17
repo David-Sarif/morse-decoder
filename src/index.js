6eb9657a4f5ba229abe6f4d5454f29870f360051
David-Sarif <DavidSarif@yandex.ru>
2020-03-18 00:08:47 +0300

    feat:solved

--- src/index.js
+++ src/index.js
@@ -35,10 +35,39 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' ',
 };
+// adding spaces to morse table
+const DECODE_TABLE ={
+    '00':'',
+    '10':'.',
+    '11':'-',
+    '**':' '
+}
 
 function decode(expr) {
-    // write your solution here
+    let arr=[];
+    // slicing all string to array by 10 symbols
+    for (let i=0;i<expr.length;i+=10){
+        arr.push(expr.substr(i,10))
+    }
+    // creating an array of dots and dashes
+    arr=arr.map(decrypt);
+
+    return arr.map(demorse).join('');
+
+
+}
+function decrypt(str){
+    let newStr=''
+    for (let i=0;i<10;i+=2){
+        key=str[i]+str[i+1]
+        newStr+=DECODE_TABLE[key]
+    }
+    return newStr;
+}
+function demorse(str){
+    return MORSE_TABLE[str]
 }
 
 module.exports = {
